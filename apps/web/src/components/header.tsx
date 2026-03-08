7bb4991e706d1597b371efda3660dc9fb4f9a7e0
YLIssakha235 <yayalibis05@gmail.com>
2026-03-08 04:15:46 +0100

    ajout des liens actifs dans hearder.tsx

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -6,15 +6,17 @@ export default function Header() {
   const links = [
     { to: "/", label: "Home" },
     { to: "/dashboard", label: "Dashboard" },
+    { to: "/incidents", label: "Incidents" },
+    { to: "/incidents/create", label: "Signaler" },
   ] as const;
 
   return (
     <div>
-      <div className="flex flex-row items-center justify-between px-2 py-1">
-        <nav className="flex gap-4 text-lg">
+      <div className="flex flex-row items-center justify-between px-4 py-2">
+        <nav className="flex gap-6 text-lg">
           {links.map(({ to, label }) => {
             return (
-              <Link key={to} to={to}>
+              <Link key={to} to={to} className="hover: underline">
                 {label}
               </Link>
             );
