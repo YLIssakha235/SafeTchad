1b706e62730b233888fa146f0f60e22b645cd61e
YLIssakha235 <yayalibis05@gmail.com>
2026-03-23 15:22:27 +0100

    correction APi après changement dans dB, lien user-incident

--- packages/api/src/routers/incident.ts
+++ packages/api/src/routers/incident.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import prisma, { IncidentType, Ville, Quartier, AxeRoutier } from "@my-better-t-app/db";
-import { publicProcedure } from "../index";
+import { publicProcedure, protectedProcedure } from "../index";
 
 const CreateIncidentInput = z.object({
   title: z.string().min(3),
@@ -12,16 +12,38 @@ const CreateIncidentInput = z.object({
 });
 
 export const incidentRouter = {
-  create: publicProcedure
+  create: protectedProcedure
     .input(CreateIncidentInput)
-    .handler(async ({ input }) => {
-      return prisma.incident.create({ data: input });
+    .handler(async ({ input, context }) => {
+      const userId = context.session?.user?.id;
+
+      if (!userId){throw new Error("Utilisateur non authentifié");}
+
+      return await prisma.incident.create({
+        data: {
+          title : input.title,
+          description : input.description,
+          type : input.type,
+          ville : input.ville,
+          quartier : input.quartier,
+          axeRoutier : input.axeRoutier,
+          reporterId : userId,
+        },
+        include: {
+          reporter: true,
+          medias: true,
+        },  
+      }); 
+     
     }),
 
-  list: publicProcedure
-    .handler(async () => {
-      return prisma.incident.findMany({
+  list: publicProcedure.handler(async () => {
+      return await prisma.incident.findMany({
         orderBy: { createdAt: "desc" },
+        include: {
+          reporter: true,
+          medias: true,
+        },
       });
     }),
 
@@ -30,8 +52,14 @@ export const incidentRouter = {
     .handler(async ({ input }) => {
       const incident = await prisma.incident.findUnique({
         where: { id: input.id },
+        include: {
+          reporter: true,
+          medias: true,
+        },
       });
+
       if (!incident) throw new Error("Incident non trouvé");
+      
       return incident;
     }),
 };
